59c0ca4a94f91254a739b2776b16723158c7931e
Anna Kostenko <anna.double.y@gmail.com>
2022-02-11 13:40:32 +0200

    Initial commit

---
+++ src/main/java/com/mycv/CVController.java
@@ -0,0 +1,35 @@
+package com.mycv;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+
+public class CVController {
+
+    @GetMapping("/examples")
+    public String showExamplePage(){
+
+        //do some stuff
+        return "examples";
+    }
+
+    @GetMapping("/profile")
+    public String showProfile(){
+        return "profile";
+    }
+
+    @GetMapping("/education")
+    public String showEducation(){
+        return "education";
+    }
+    @GetMapping("/lang")
+    public String showLanguages(){
+        return "lang";
+    }
+    @GetMapping("/superpowers")
+    public String showSuperpowers(){
+        return "superpowers";
+    }
+}
